8e5b0bafd50501d062ecd92f1eda1a8ce1ee871b
Josh Beatty <joshuatb6@gmail.com>
2022-07-05 17:56:38 +0000

    Moving rimraf to build script

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,18 +1,20 @@
 const esbuild = require('esbuild')
-
+const rimraf = require('rimraf')
 // Automatically exclude all node_modules from the bundled version
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 console.time('⚡ build');
-esbuild.build({
-    entryPoints: ['./src/index.ts'],
-    outfile: 'lib/index.js',
-    bundle: true,
-    minify: true,
-    platform: 'node',
-    sourcemap: true,
-    target: 'node16',
-    plugins: [nodeExternalsPlugin()]
-}).catch(() => process.exit(1)).then(() => {
-    console.timeEnd('⚡ build');
-    console.log()
-});
+rimraf('./lib/', () => {
+    esbuild.build({
+        entryPoints: ['./src/index.ts'],
+        outfile: 'lib/index.js',
+        bundle: true,
+        minify: true,
+        platform: 'node',
+        sourcemap: true,
+        target: 'node16',
+        plugins: [nodeExternalsPlugin()]
+    }).catch(() => process.exit(1)).then(() => {
+        console.timeEnd('⚡ build');
+        console.log()
+    });
+})
